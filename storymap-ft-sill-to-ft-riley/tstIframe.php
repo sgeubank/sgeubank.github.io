8ce0058599cfc7f9e45a9086f336a1ef31b80525
Stephen Eubank <sgeubank@earthlink.net>
2024-04-05 20:50:04 -0400

    Adding html iframe test`

---
+++ storymap-ft-sill-to-ft-riley/tstIframe.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>AJAX-ZOOM in iframe</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="imagetoolbar" content="no">
+<meta name="robots" content="noindex,nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<link rel="canonical" href="https://www.ajax-zoom.com/examples/example33_vario.php" />
+
+<!-- jQuery core, needed! -->
+<script src="../axZm/plugins/jquery-1.8.3.min.js"></script>
+
+<!-- AJAX-ZOOM core -->
+<link rel="stylesheet" type="text/css" href="../axZm/axZm.css" media="all" />
+<link href="../axZm/themes/white/axZm_white.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="../axZm/jquery.axZm.js"></script>
+
+</head>
+<body>
+
+<iframe
+	class="ajaxzoom_iframe lazy"
+	width="100%"
+	frameborder="0"
+	id="ajaxzoom_frame_5"
+	data-original="example33_vario.php?zoomData=/pic/zoom/furniture/furniture_006.jpg&mouseScrollEnable=1&iframeID=ajaxzoom_frame_5"
+	scrolling="no"
+	allowfullscreen>
+</iframe>
+</body>
+</html>
